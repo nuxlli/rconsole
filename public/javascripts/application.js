32f48f1d24ebeb3407515c2f9d8eb2b4f2f0031d
Everton Ribeiro <nuxlli@gmail.com>
2011-11-08 18:26:38 -0200

    First version using termlib

---
+++ public/javascripts/application.js
@@ -0,0 +1,49 @@
+;(function($) {
+    var helpPage = ["oiee!!"];
+
+    function myTermHandler() {
+        console.log(this.inputChar);
+        // var line = this.lineBuffer;
+        // this.newLine();
+        // if (line == "/help") {
+        //     this.write(helpPage);
+        //     this.statusLine("status: show help", 1)
+        // }
+        // else if (line == "exit") {
+        //     this.close();
+        //     return;
+        // }
+        // else if (line != "") {
+        //     var $self = this;
+        //     setTimeout(function() {
+        //         $self.newLine();
+        //         $self.write("You typed: %c(#f17caa)" + line + "%c0");
+        //         $self.prompt();
+        //     }, 2000);
+        // }
+        // this.prompt();
+    }
+    
+    function myCtrlHandler() {
+        console.log(this.inputChar);
+    }
+
+    var term;
+    $(document).ready(function() {
+        term = new Terminal({
+            termDiv: "terminal",
+            greeting: [
+                "%c(green)*** tvconsole 0.1 ***%c0",
+                "type: /help to show help use"
+            ],
+            charMode: true,
+            closeOnESC: false,
+            ps: "➜",
+            handler: myTermHandler,
+            // ctrlHandler: myCtrlHandler
+        });
+
+        term.open();
+    });
+})(jQuery);
+
---
+++ server.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    app     = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: '4pKbjaoWAViMzGxOEIWMO7CzDoYiSwRdi5kEZne2lPaKlU0soqWwj4CjMI7yxVMeZaYXSgPnB8w5NzDKJ0l87dYzzF2ZiSa7UolPnaobpExRW6DqNQaCHUnq8AzfJ9EwnNbk1s2_UriW_CyCTsjmbx'
+  }));
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Socket.IO
+require('./lib/sockets').init(app);
+
+// Routes
+require('./lib/routes').init(app);
+
+// Main
+app.listen(process.env.PORT || 3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
